624b6c95e05b4a1f65f3acd7319e10825285dae9
greg2010 <greg2008200@gmail.com>
2019-04-27 12:49:14 +0300

    Add project files.

---
+++ MiningOS/Program.cs
@@ -0,0 +1,212 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        private IMyRemoteControl mainController = null;
+        private Dictionary<Base6Directions.Direction, List<IMyThrust>> directionalThrusters = null;
+        private GyroController gyroController = null;
+
+        private IMyRemoteControl getMainController()
+        {
+            var controllers = new List<IMyRemoteControl>();
+            GridTerminalSystem.GetBlocksOfType<IMyRemoteControl>(controllers);
+            controllers = controllers.Where(controller => controller.CustomName.Contains("Main")).ToList();
+            if (controllers.Count != 1)
+            {
+                Echo($"{controllers.Count} main remote controllers found, exiting...");
+                throw new Exception();
+            }
+            return controllers[0];
+        }
+
+        private Dictionary<Base6Directions.Direction, List<IMyThrust>> getDirectionalThrusters(IMyCubeBlock reference)
+        {
+            Dictionary<Base6Directions.Direction, List<IMyThrust>> thrustersDict = new Dictionary<Base6Directions.Direction, List<IMyThrust>>();
+            List<IMyThrust> thrusters = new List<IMyThrust>();
+            GridTerminalSystem.GetBlocksOfType<IMyThrust>(thrusters);
+            foreach (var thruster in thrusters)
+            {
+                var direction = reference.Orientation.TransformDirectionInverse(Base6Directions.GetFlippedDirection(thruster.Orientation.Forward));
+                if (!thrustersDict.ContainsKey(direction))
+                {
+                    thrustersDict.Add(direction, new List<IMyThrust>());
+                }
+                thrustersDict[direction].Add(thruster);
+            }
+            return thrustersDict;
+        }
+
+        private List<IMyGyro> getGyros()
+        {
+            List<IMyGyro> gyroList = new List<IMyGyro>();
+            GridTerminalSystem.GetBlocksOfType<IMyGyro>(gyroList);
+            return gyroList;
+        }
+
+        public Program()
+        {
+            mainController = getMainController();
+            directionalThrusters = getDirectionalThrusters(mainController);
+            gyroController = new GyroController(getGyros(), this.Echo);
+            gyroController.Schedule(new GyroController.Job(new Vector3D(-425.34d, -166.88d, 917.13d), this.mainController));
+            gyroController.Schedule(new GyroController.Job(new Vector3D(-190.29, -47.17, 1049.49), this.mainController));
+            
+
+            foreach (var tg in directionalThrusters)
+            {
+                Echo($"Direction: {tg.Key} count: {tg.Value.Count} ");
+            }
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+
+
+
+
+        public void Save()
+        {
+
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            gyroController.Tick();
+        }
+
+
+        class GyroController
+        {
+            public struct Job
+            {
+                public Vector3D coordinates;
+                public IMyCubeBlock reference;
+                public Job(Vector3D coordinates, IMyCubeBlock reference)
+                {
+                    this.coordinates = coordinates;
+                    this.reference = reference;
+                }
+            }
+
+            public List<IMyGyro> gyroList;
+            private List<Job> jobList = new List<Job>();
+            Action<string> Echo;
+
+            public GyroController(List<IMyGyro> gyroList, Action<string> Echo)
+            {
+                this.gyroList = gyroList;
+                this.Echo = Echo;
+            }
+
+            public void Schedule(Job job)
+            {
+                jobList.Add(job);
+            }
+
+            public void Tick()
+            {
+
+                if (jobList.Count > 0)
+                {
+                    Job curJob = jobList.First();
+                    Echo($"GyroController: Found job with coords {curJob.coordinates.ToString()}");
+                    var curPosn = curJob.reference.GetPosition();
+                    Vector3D directionVec = curJob.coordinates - curPosn;
+                    double pitch, yaw = 0;
+                    VectorMath.GetRotationAngles(directionVec, curJob.reference.WorldMatrix.Forward, curJob.reference.WorldMatrix.Left, curJob.reference.WorldMatrix.Up, out yaw, out pitch);
+                    Echo($"Pitch: {pitch} Yaw: {yaw}");
+                    var originalRotVec = new Vector3D(-pitch, yaw, 0);
+                    int gyrosCompleted = 0;
+                    foreach (var gyro in gyroList)
+                    {
+                        var originalWorldRotVec = VectorMath.GetWorldDirection(originalRotVec, curJob.reference);
+                        var shiftedGyroRotVector = VectorMath.GetBodyDirection(originalWorldRotVec, gyro);
+                        float pitchRounded = (float)Math.Round(shiftedGyroRotVector.X, 2);
+                        float yawRounded = (float)Math.Round(shiftedGyroRotVector.Y, 2);
+                        float rollRounded = (float)Math.Round(shiftedGyroRotVector.Z, 2);
+
+                        Echo($"PitchSpeed: {pitchRounded} YawSpeed: {yawRounded} RollSpeed: {rollRounded}");
+                        if (pitchRounded == 0f && yawRounded == 0f && rollRounded == 0f)
+                        {
+                            gyro.GyroOverride = false;
+                            gyrosCompleted++;
+                        }
+                        else
+                        {
+                            gyro.Pitch = pitchRounded;
+                            gyro.Yaw = yawRounded;
+                            gyro.Roll = rollRounded;
+                            gyro.GyroOverride = true;
+                        }
+                    }
+                    if (gyrosCompleted == gyroList.Count)
+                    {
+                        Echo($"Job with coords {curJob.coordinates.ToString()} completed, popping");
+                        jobList.Remove(curJob);
+
+                    }
+                }
+            }
+
+        }
+
+        class PID
+        {
+            private double kP;
+            private double kI;
+            private double kD;
+
+            private double timeStep;
+
+            private double prevError;
+
+            private double cP;
+            private double cI;
+            private double cD;
+
+
+            public PID(double kP, double kI, double kD, double timeStep)
+            {
+                this.kP = kP;
+                this.kI = kI;
+                this.kD = kD;
+                this.timeStep = timeStep;
+                this.Reset();
+            }
+
+            public void Reset()
+            {
+                this.prevError = 0d;
+                this.cP = 0d;
+                this.cI = 0d;
+                this.cD = 0d;
+            }
+
+            public double Update(double error)
+            {
+                var deltaError = error - this.prevError;
+                this.cP = error;
+                this.cI = error * timeStep;
+                this.cD = timeStep != 0 ? deltaError / timeStep : 0;
+                this.prevError = error;
+                return (this.kP * this.cP) + (this.kI * this.cI) + (this.kD * this.cD);
+            }
+        }
+    }
+}
---
+++ MiningOS/VectorMath.cs
@@ -0,0 +1,75 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        // Inspired by Whip's scripts
+        public class VectorMath
+        {
+
+            public static Vector3D GetBodyDirection(Vector3D worldDirection, IMyCubeBlock reference)
+            {
+                return Vector3D.TransformNormal(worldDirection, MatrixD.Transpose(reference.WorldMatrix));
+            }
+
+            public static Vector3D GetWorldDirection(Vector3D bodyDirection, IMyCubeBlock reference)
+            {
+                return Vector3D.TransformNormal(bodyDirection, reference.WorldMatrix);
+            }
+
+            public static void GetRotationAngles(Vector3D vTarget, Vector3D vFront, Vector3D vLeft, Vector3D vUp, out double yaw, out double pitch)
+            {
+                var projectTargetUp = VectorProjection(vTarget, vUp);
+                var projTargetFrontLeft = vTarget - projectTargetUp;
+
+                yaw = VectorAngleBetween(vFront, projTargetFrontLeft);
+                pitch = VectorAngleBetween(vTarget, projTargetFrontLeft);
+
+                //---Check if yaw angle is left or right   
+                //multiplied by -1 to convert from right hand rule to left hand rule 
+                yaw = -1 * Math.Sign(vLeft.Dot(vTarget)) * yaw;
+
+                //---Check if pitch angle is up or down     
+                pitch = Math.Sign(vUp.Dot(vTarget)) * pitch;
+
+                //---Check if target vector is pointing opposite the front vector 
+                if (pitch == 0 && yaw == 0 && vTarget.Dot(vFront) < 0)
+                {
+                    yaw = Math.PI;
+                }
+            }
+
+            public static Vector3D VectorProjection(Vector3D a, Vector3D b) //proj a onto b    
+            {
+                Vector3D projection = a.Dot(b) / b.LengthSquared() * b;
+                return projection;
+            }
+
+            public static double VectorAngleBetween(Vector3D a, Vector3D b) //returns radians  
+            {
+                if (a.LengthSquared() == 0 || b.LengthSquared() == 0)
+                    return 0;
+                else
+                    return Math.Acos(MathHelper.Clamp(a.Dot(b) / a.Length() / b.Length(), -1, 1));
+            }
+        }
+    }
+}
